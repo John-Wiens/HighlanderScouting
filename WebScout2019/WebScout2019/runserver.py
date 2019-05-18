ac0d83225ae6ec2519d9e2b09fdc40a437f4dd49
John-Wiens <jwiens3141@gmail.com>
2019-05-18 10:33:15 -0600

    Patched Repository for Security Vulnerabilities

--- WebScout2019/WebScout2019/runserver.py
+++
@@ -1,9 +0,0 @@
-"""
-This script runs the WebScout2019 application using a development server.
-"""
-
-from os import environ
-from WebScout2019 import app
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',threaded=True)
